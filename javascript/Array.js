a14e4c9a2393877be155cf3871e88c8c4e60338d
HugoEmanuelLC <helc85.info@gmail.com>
2023-09-20 10:43:17 +0200

    creation de biblioteque

---
+++ javascript/Array.js
@@ -0,0 +1,7 @@
+let string = 'HugoLClavinas' //variable type string
+
+let array = string.split('L'); //tableau de 2 valeurs separer par la place du "L"
+console.log(array);
+
+array = string.split('') //letres du string divise en tableau par lettre
+console.log(array);
